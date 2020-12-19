f450e29dd3dab2254d3d32e5eeecd8cdcf1ea759
saenalcha <saenalcha@gmail.com>
2020-12-19 15:59:02 +0900

    feat: Add eslint airbnb-typescript

--- layout/Default.tsx
+++ layout/Default.tsx
@@ -1,8 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import Link from 'next/link'
 
 const DefaultLayout = ({ children }) => {
   return (
     <div>
+      <nav>
+        <Link href='/'>
+          <a style={{marginRight: '4px'}}>
+            home
+          </a>
+        </Link>
+        <Link href='/about'>
+          about
+        </Link>
+      </nav>
       {children}
     </div>
   )
