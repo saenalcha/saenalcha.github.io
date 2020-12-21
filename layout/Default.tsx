99500002a74da48b96ff8fce3b881c32b4a93538
saenalcha <saenalcha@gmail.com>
2020-12-21 21:23:54 +0900

    Add emotion/styled

--- layout/Default.tsx
+++ layout/Default.tsx
@@ -1,15 +1,39 @@
 import React from "react";
+import { useRouter } from 'next/router'
 import Link from "next/link";
+import styled from '@emotion/styled'
 
 const DefaultLayout = ({ children }) => {
+  const router = useRouter()
+
+  const Nav = styled.nav`
+    margin: 20px;
+  `
+  const Linker = ({ href, name }) => {
+    const Anchor = styled.a`
+      margin-right: 8px;
+      cursor: pointer;
+      color: ${router.pathname === href ? '#0070f3' : 'inherit'};
+    `
+    return (
+      <Link href={href}>
+        <Anchor>{name}</Anchor>
+      </Link>
+    )
+  }
+
   return (
     <div>
-      <nav>
-        <Link href="/">
-          <a style={{ marginRight: "4px" }}>home</a>
-        </Link>
-        <Link href="/about">about</Link>
-      </nav>
+      <Nav>
+        <Linker
+          name={"home"}
+          href={"/"}
+        />
+        <Linker
+          name={"about"}
+          href={"/about"}
+        />
+      </Nav>
       {children}
     </div>
   );
