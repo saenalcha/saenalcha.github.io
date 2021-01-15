ee2be9ddb7d6f49a2d902677ff3e9d6aa6d5348f
saenalcha <saenalcha@gmail.com>
2021-01-15 23:22:43 +0900

    feat: Add Posts page content

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -9,7 +9,25 @@ const Posts = () => {
             </h2>
             <div>
               <p>
-
+                에러코드:
+              </p>
+              <p>
+                <samp>
+                  Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client
+                </samp>
+              </p>
+              <p>
+                해결책:
+              </p>
+              <p>
+                <code>
+                  <p>
+                    ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY [password];
+                  </p>
+                  <p>
+                    FLUSH PRIVILEGES;
+                  </p>
+                </code>
               </p>
             </div>
           </div>
